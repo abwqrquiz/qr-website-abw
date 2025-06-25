af8fdbe8da9115ec981e91ab0f2418046484290e
abwqrquiz <jan.tietjen@outlook.com>
2025-06-25 06:46:44 +0200

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,3 +1,5 @@
+var questionNumber = "Test";
+
 function createCookie(name,value,days) {
 	if (days) {
 		var date = new Date();
@@ -19,14 +21,50 @@ function readCookie(name) {
 	return null;
 }
 
+function getCookie(name) {
+    var cVal = document.cookie.match('(?:^|;) ?' + name + '=([^;]*)(?:;|$)');
+    if (!cVal) {
+      return "";
+    } else {
+      return cVal[1];
+    }
+  }
+
+
 function eraseCookie(name) {
 	createCookie(name,"",-1);
 }
 
 function toggleMenu() {
-        document.getElementById("navbar").classList.toggle("active");
-    }
+    document.getElementById("navbar").classList.toggle("active");
+}
 	
-	function resultVisibility() {
-		document.getElementById("result").style.visibility = "visible";
-} 
+function resultSubmitted() {
+	document.getElementById("result").style.visibility = "visible";
+		
+	var correct = document.querySelector('input[name=options]:checked').value;
+		
+	var cookie = getCookie("Test");
+	
+	
+	if (!cookie){
+		if (correct == 1) {
+			document.getElementById("correct").style.Color = "Green";
+			document.getElementById("correct").style.display = "block";
+			document.getElementById("false").style.display = "none";
+			document.getElementById("2ndTry").style.display = "none";
+			
+			createCookie(questionNumber, 1, 5);
+		} else if (correct == 0) {
+			document.getElementById("false").style.Color = "Red";
+			document.getElementById("false").style.display = "block";
+			document.getElementById("correct").style.display = "none";
+			document.getElementById("2ndTry").style.display = "none";
+			
+			createCookie(questionNumber, 0, 5);
+		}
+	} else if (cookie == 1) {
+		document.getElementById("2ndTry").style.display = "block";
+		alert("Cookie exists");
+	}
+}

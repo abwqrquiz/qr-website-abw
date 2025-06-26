f123b65ffc0b0dff751680ce1e2526d03bcb1da8
abwqrquiz <jan.tietjen@outlook.com>
2025-06-26 09:55:28 +0200

    Updated

---
+++ index.js
@@ -0,0 +1,26 @@
+if (localStorage.getItem('quizFailed') === 'true') {
+    document.getElementById('quiz-container').style.display = 'none';
+    document.getElementById('message').innerText = 'Sie haben das Quiz bereits falsch beantwortet. Leider können Sie es nicht erneut versuchen.';
+}
+
+function answerQuestion(questionNumber, isCorrect) {
+    if (localStorage.getItem('quizFailed') === 'true') {
+        return;
+    }
+
+    if (isCorrect) {
+        alert('Richtig!');
+        document.getElementById('quizbuttons').style.property = backgroundColor = 'green';
+        document.getElementById('message').innerText = 'Richtig!';
+    } else {
+        window.location.replace("quizfail.html");
+        localStorage.setItem('quizFailed', 'true');
+
+        document.addEventListener('DOMContentLoaded', function () {
+            document.getElementById('failed-container').style.display = 'block';
+        });
+
+        document.getElementsById('quiz-container').style.display = 'none';
+        // document.getElementById('message').innerText = 'Sie haben das Quiz bereits falsch beantwortet. Leider können Sie es nicht erneut versuchen.';
+    }
+}

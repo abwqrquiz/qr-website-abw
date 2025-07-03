25c2fc91cba5631af85efa12fbc02a67453f26f0
abwqrquiz <jan.tietjen@outlook.com>
2025-07-03 08:11:45 +0200

    Update

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-if (localStorage.getItem('quizFailed') === 'true') {
-    document.getElementById('quiz-container').style.display = 'none';
-    document.getElementById('message').innerText = 'Sie haben das Quiz bereits falsch beantwortet. Leider können Sie es nicht erneut versuchen.';
-}
-
 function answerQuestion(questionNumber, isCorrect) {
     if (localStorage.getItem('quizFailed') === 'true') {
-        return;
+        /*         document.addEventListener('DOMContentLoaded', function () {
+                    document.getElementById('failed-container').style.display = 'block';
+                }); */
+
+        document.getElementsById('quiz-container').style.display = 'none';
     }
 
     if (isCorrect) {
@@ -16,13 +15,14 @@ function answerQuestion(questionNumber, isCorrect) {
         window.location.replace("quizfail.html");
         localStorage.setItem('quizFailed', 'true');
 
-        document.addEventListener('DOMContentLoaded', function () {
-            document.getElementById('failed-container').style.display = 'flex';
-        });
-
-        document.getElementsById('quiz-container').style.display = 'none';
+        document.getElementById('failed-container').style.display = 'block';
         // document.getElementById('message').innerText = 'Sie haben das Quiz bereits falsch beantwortet. Leider können Sie es nicht erneut versuchen.';
     }
+}
+
+if (localStorage.getItem('quizFailed') === 'true') {
+    document.getElementById('quiz-container').style.display = 'none';
+    document.getElementById('message').innerText = 'Sie haben das Quiz bereits falsch beantwortet. Leider können Sie es nicht erneut versuchen.';
 }
 
 function saveAnswer() {
